b777de409991b42e3531f3b77013cb31dc50d597
Petr Safarcik <petr.safarcik@gmail.com>
2016-10-16 13:21:11 +0200

    Change folder structure

---
+++ Exercises/exercise_3_3.c
@@ -0,0 +1,21 @@
+// exercise_3_3.c
+
+/*
+
+	Write a program that sounds an alert and then prints 
+	the following text:
+
+	Startled by the sudden sound, Sally shouted,
+	"By the Great Pumpkin, what was that!"
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\a\n");
+	printf("„Startled by the sudden sound, Sally\nshouted,\n\"By the Great Pumpking, what was that!\"\n");
+
+	return 0;
+}
---
+++ Exercises/exercise_3_7.c
@@ -0,0 +1,10 @@
+// exercise_3_7.c
+
+/* 
+
+	There are 2.54 centimeters to the inch. Write a program that asks you to enter 
+	your height in inches and then displays your height in centimeters. Or, if you prefer, 
+	ask for the height in centimeters and convert that to inches.
+
+*/
+
